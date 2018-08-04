fd6ee81fa8c3e1a1e32502be9440e152292b8ec0
hotvit <huanthps06341@fpt.edu.vn>
2018-08-04 18:15:31 +0700

    calculator your height

---
+++ src/cal.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ps06341
+ */
+public class cal {
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
+        System.out.print("Nhap chieu cao cua ban: ");
+        String tall = key.nextLine();
+        
+        System.out.println("Chieu cao cua ban la: "+tall);
+    }
+}
